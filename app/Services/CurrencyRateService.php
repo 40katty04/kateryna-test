4363fe36a473c6c484868026719953cb59bce446
Kateryna Vlasenko <40katty04@gmail.com>
2023-05-28 18:26:09 +0200

    add error catching to currency rate service

--- app/Services/CurrencyRateService.php
+++ app/Services/CurrencyRateService.php
@@ -3,20 +3,29 @@
 namespace App\Services;
 
 use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Log;
+use Exception;
+
 class CurrencyRateService
 {
     public static function getPrice()
     {
-        $client = new Client();
-        $key = env('FAST_FOREX_TRIAL_API_KEY');
+        try {
+            $client = new Client();
+            $key = env('FAST_FOREX_TRIAL_API_KEY');
 
-        $result = $client->get('https://api.fastforex.io/fetch-one?from=BTC&to=UAH&api_key=' . $key , [
-            'headers' => [
-                'accept'=> 'application/json',
-            ],
-        ]);
-        $result = json_decode($result->getBody());
+            $result = $client->get('https://api.fastforex.io/fetch-one?from=BTC&to=UAH&api_key=' . $key, [
+                'headers' => [
+                    'accept' => 'application/json',
+                ],
+            ]);
+            $result = json_decode($result->getBody());
 
-        return $result->result->UAH;
+            return $result->result->UAH;
+        } catch (Exception $exception){
+            Log::error('Get rate error: '. $exception->getMessage());
+
+            return 0;
+        }
     }
 }
